c1c8022db51d17662edfee240b6f9a4a865001c4
TimVanHal <tim.vanhal@student.ap.be>
2015-06-18 10:09:25 +0200

    removing accidental duplicate

--- html/js/swap.js
+++
@@ -1,5 +0,0 @@
-function swap(one, two, three) {
-    document.getElementById(one).style.display = 'inline-block';
-    document.getElementById(two).style.display = 'none';
-    document.getElementById(three).style.display = 'none';
-}
